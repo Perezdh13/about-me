57988ab3d07aa2b6541a1a2eba6010db3393eff0
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-02-03 18:51:55 +0100

    firt commit

---
+++ src/javascript/App.js
@@ -0,0 +1,13 @@
+import Mainpage from "./MainPage";
+
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Mainpage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/javascript/MainPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import background from '../img/Fondo pagina.png'
+import bigInitial from '../img/P mayuscula.png' 
+
+function MainPage() {
+  return (
+    <div className='main-page'>
+        <img src= {background}/>
+      <div className='main-page-icon'>
+        
+      </div>
+      <div className='main-page-bar'>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default MainPage
